2bafccfb378a0a522779a1a9f0c3124cad0956ff
DhyanTD <69795325+DhyanTD@users.noreply.github.com>
2024-12-26 22:37:21 +0530

    Initial commit

---
+++ src/log.ts
@@ -0,0 +1,3 @@
+import { log as defaultLogger } from "@smoke-trees/postgres-backend";
+
+export const log = defaultLogger;
---
+++ src/settings.ts
@@ -0,0 +1,25 @@
+import { Settings } from "@smoke-trees/postgres-backend";
+
+export class ApplicationSettings extends Settings {
+  databaseType: "postgres" | "mysql";
+  dbPassword: string;
+  dbUser: string;
+  dbHost: string;
+  dbPort: string | undefined;
+  database: string;
+
+  constructor() {
+    super();
+    this.databaseType = "postgres";
+    this.dbPassword = this.getValue("PGPASSWORD", "mysecretpassword");
+    this.dbUser = this.getValue("PGUSER", "postgres");
+    this.dbHost = this.getValue("PGHOST", "localhost");
+    this.dbPort = this.getValue("PGPORT", "5432");
+    this.database = this.getValue("PGDATABASE", "postgres");
+    super();
+  }
+}
+
+const settings = new ApplicationSettings();
+
+export default settings;
